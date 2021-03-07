8fd55cb62550f08819d2849ea0d8af41fbc74f31
Kashif <kashifqamar111@gmail.com>
2021-03-07 23:56:34 +0200

    Added calendar, profile and contacts

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import {Text, View} from 'native-base';
 import CustomButton from '../components/CustomButton';
+import {Calendar} from 'react-native-plain-calendar';
+import {StyleSheet} from 'react-native';
 
 const Home = () => {
   return (
     <View>
-      <CustomButton title="Quick Assesment" />
-      <CustomButton title="View Assesment" />
+      <View style={styles.button}>
+        <CustomButton title="Quick Assesment" />
+        <CustomButton title="View Assesment" />
+      </View>
+
+      <View style={styles.calendar}>
+        <Calendar />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  calendar: {
+    marginTop: '30%',
+  },
+  button: {
+    marginTop: '10%',
+  },
+});
 
 export default Home;
--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,7 +1,31 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { Text } from 'native-base';
+import {Text, View} from 'native-base';
+import {StyleSheet} from 'react-native';
+import CustomButton from '../components/CustomButton';
 
-const Profile = () => <Text>Profile</Text>;
+const Profile = () => {
+  return (
+    <View style={{marginTop: '30%'}}>
+      <Text style={styles.Text}>Profile</Text>
+      <Text style={styles.Text}>John Doe</Text>
+      <Text style={styles.Text}>Points: 150</Text>
+      <Text style={styles.Text}>Surveys Taken: 7</Text>
+      <CustomButton title="Use Points" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  Text: {
+    textAlign: 'center',
+    color: '#023e8a',
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginLeft: 10,
+    marginBottom: 10,
+    marginTop: 10,
+  },
+});
 
 export default Profile;
