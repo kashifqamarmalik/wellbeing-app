2c7041302f0cb22e31a59d04feb18667c2aa7f8a
NhanNguyen <ntnhan240696@gmail.com>
2021-03-12 18:28:42 +0200

    Ui profile is ready

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,30 +1,105 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {Text, View} from 'native-base';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, View, Text, ScrollView, SafeAreaView, Image, Dimensions} from 'react-native';
 import CustomButton from '../components/CustomButton';
+// import {Chart, VerticalAxis, HorizontalAxis, Line} from 'react-native-responsive-linechart'
+import {Chart} from 'react-native-responsive-linechart/lib/Chart'
+import {VerticalAxis} from 'react-native-responsive-linechart/lib/VerticalAxis'
+import {HorizontalAxis} from 'react-native-responsive-linechart/lib/HorizontalAxis'
+import {Line} from 'react-native-responsive-linechart/lib/Line'
 
+const data1 = [
+  {x: 0, y: 1},
+  {x: 1, y: 7},
+  {x: 8, y: 13},
+  {x: 9, y: 11.5},
+  {x: 10, y: 12}
+]
+
+const data2 = [
+  {x: 0, y: 15},
+  {x: 1, y: 10},
+  {x: 2, y: 12},
+  {x: 3, y: 7},
+  {x: 8, y: 12},
+  {x: 9, y: 13.5},
+  {x: 10, y: 18}
+]
 const Profile = () => {
   return (
-    <View style={{marginTop: '30%'}}>
-      <Text style={styles.Text}>Profile</Text>
-      <Text style={styles.Text}>John Doe</Text>
-      <Text style={styles.Text}>Points: 150</Text>
-      <Text style={styles.Text}>Surveys Taken: 7</Text>
-      <CustomButton title="Use Points" />
-    </View>
+    <SafeAreaView style={{backgroundColor: "white"}} >
+      <ScrollView style={styles.scrollView}>
+        <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}} />
+        <Text style={styles.name}>John Doe</Text>
+        <View style={styles.statistics}>
+          <View style={styles.statisticsItem}>
+            <Text style={styles.text}>Current Points</Text>
+            <Text style={styles.text}>150</Text>
+          </View>
+          <View style={styles.statisticsItem}>
+            <Text style={styles.text}>Surveys Taken</Text>
+            <Text style={styles.text}>7</Text>
+          </View>
+        </View>
+        <CustomButton title="         Use Points         " />
+        <View style={{marginTop: "10%"}}>
+          <Chart
+            style={{height: "60%", width: '100%', backgroundColor: '#eee'}}
+            xDomain={{min: 0, max: 10}}
+            yDomain={{min: 0, max: 20}}
+            padding={{left: 20, top: 20, bottom: 20, right: 20}}
+          >
+            <VerticalAxis tickValues={[0, 4, 8, 12, 16, 20]} />
+            <HorizontalAxis tickCount={3} />
+            <Line data={data1} smoothing="none" theme={{stroke: {color: 'red', width: 1.5}}} />
+            <Line data={data2} smoothing="none" theme={{stroke: {color: 'blue', width: 1.5}}} />
+          </Chart>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  Text: {
-    textAlign: 'center',
+  scrollView: {
+    marginHorizontal: 20,
+    height: "100%"
+  },
+  avatar: {
+    width: "53%",
+    height: "30%",
+    borderRadius: 500,
+    borderWidth: 4,
+    borderColor: "white",
+    position: 'absolute',
+    marginTop: "7%",
+    zIndex: 2,
+  },
+  name: {
     color: '#023e8a',
     fontWeight: 'bold',
+    fontSize: 30,
+    position: 'absolute',
+    marginTop: "35%",
+    zIndex: 2,
+    right: "5%"
+  },
+  statistics: {
+    flex: 1,
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: "black",
+    marginVertical: 10,
+    marginTop: "50%",
+  },
+  statisticsItem: {
+    width: "50%",
+    paddingVertical: "10%",
+  },
+  text: {
+    textAlign: 'center',
+    color: '#023e8a',
     fontSize: 20,
-    marginLeft: 10,
-    marginBottom: 10,
-    marginTop: 10,
   },
 });
 
