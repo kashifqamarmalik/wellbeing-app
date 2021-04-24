d79ffc5fe2585f5af72825b0506b75b6108f2f15
Kashif <kashifqamar111@gmail.com>
2021-04-25 00:12:33 +0300

    quiz functionality

--- src/views/HomeQuiz.js
+++ src/views/HomeQuiz.js
@@ -1,8 +1,10 @@
-import {Text, View} from 'native-base';
-import {StyleSheet} from 'react-native';
+import {Button, Text, View} from 'native-base';
+import {StyleSheet, TouchableOpacity} from 'react-native';
 import CustomButton from '../components/CustomButton';
-import Assesment from '../data/Assessment';
+import {putCompletedAssessment} from '../api/AssessmentAPI';
+import {Assessment} from '../data/Assessment';
 import React, {useEffect, useState} from 'react';
+import RadioButtonRN from 'radio-buttons-react-native';
 
 const HomeQuiz = () => {
   const [quiz, setQuiz] = useState();
@@ -22,7 +24,7 @@ const HomeQuiz = () => {
       .then((res) => {
         setQuiz(res);
         setLoaded(true);
-        console.log('fetched quiz', quiz);
+        //console.log('fetched quiz', quiz);
         //q1 = quiz.questions[0].question_string;
         setQ1(quiz.questions[0].question_string);
         setMax1(quiz.questions[0].max_text);
@@ -38,18 +40,171 @@ const HomeQuiz = () => {
 
   useEffect(() => {
     getData();
-  });
+  }, [quiz]);
+
+  const op1 = [
+    {
+      label: max1,
+    },
+    {
+      label: min1,
+    },
+  ];
+
+  const op2 = [
+    {
+      label: max2,
+    },
+    {
+      label: min2,
+    },
+  ];
+
+  const ass = {
+    comment: 'This assessment came from the app',
+    user_id: '608041de2abcce6f6cc2f72b',
+    assessment_id: '6026848f720e2f5db8c09ca9',
+    answers: [
+      {
+        score: {
+          $numberDecimal: '-1',
+        },
+        question_id: '60268600a5369fd7c2e0e19f',
+        question_string: 'How are you feeling?',
+        min_score: {
+          $numberDecimal: '0',
+        },
+        max_score: {
+          $numberDecimal: '10',
+        },
+        max_text: 'Good',
+        min_text: 'Bad',
+      },
+      {
+        score: {
+          $numberDecimal: '10',
+        },
+        question_id: '6026876aa5369fd7c2e0e1a0',
+        question_string: 'How is your workload?',
+        min_score: {
+          $numberDecimal: '0',
+        },
+        max_score: {
+          $numberDecimal: '10',
+        },
+        max_text: 'Heavy',
+        min_text: 'Low',
+      },
+    ],
+  };
+
+  async () => {
+    let assessment = new Assessment(
+      '6026848f720e2f5db8c09ca8',
+      [],
+      'Hello from app',
+    );
+    assessment.setUserId('606c9a4094c4d13c0cbfd43a');
+    assessment.setAssessmentName('Quick Assessment');
+    let res = await putCompletedAssessment(assessment);
+    let json = await res.json();
+    console.log('json', json);
+  };
 
   return (
     <View>
-      <Text>{q1}</Text>
-      <Text>{max1}</Text>
-      <Text>{min1}</Text>
-      <Text>{q2}</Text>
-      <Text>{max2}</Text>
-      <Text>{min2}</Text>
+      <Text style={styles.question}>{q1}</Text>
+      <RadioButtonRN data={op1} selectedBtn={(e) => console.log(e)} />
+      <Text style={styles.question}>{q2}</Text>
+      <RadioButtonRN data={op2} selectedBtn={(e) => console.log(e)} />
+      <CustomButton
+        title="Submit"
+        onPress={() => {
+          let assessment = new Assessment(
+            '6026848f720e2f5db8c09ca8',
+            [],
+            'Hello from app',
+          );
+          assessment.setUserId('606c9a4094c4d13c0cbfd43a');
+          assessment.setAssessmentName('Quick Assessment');
+          let res = putCompletedAssessment(assessment);
+          let json = res.json();
+          console.log('json', json);
+        }}></CustomButton>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingLeft: '15%',
+    paddingTop: '7%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  },
+  viewButton: {
+    backgroundColor: '#ade8f4',
+    height: 50,
+    width: '60%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    borderWidth: 3,
+    borderColor: '#000000',
+    marginTop: '5%',
+  },
+  button: {
+    shadowColor: 'rgba(0,0,0, .4)', // IOS
+    shadowOffset: {height: 1, width: 1}, // IOS
+    shadowOpacity: 1, // IOS
+    shadowRadius: 1, //IOS
+    backgroundColor: '#ade8f4',
+    elevation: 2, // Android
+    height: 50,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    borderRadius: 10,
+    borderWidth: 3,
+    borderColor: '#000000',
+    marginTop: 10,
+  },
+  surveyTitle: {
+    color: '#023e8a',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  question: {
+    textAlign: 'center',
+    color: '#023e8a',
+    fontWeight: 'bold',
+    fontSize: 23,
+    marginTop: '10%',
+  },
+  nameText: {
+    textAlign: 'left',
+    color: '#023e8a',
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginLeft: 10,
+    marginBottom: 10,
+  },
+  roundButton1: {
+    width: 90,
+    height: 90,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 100,
+    backgroundColor: 'orange',
+    position: 'absolute',
+    right: 0,
+    marginTop: 50,
+    margin: 15,
+  },
+});
 
 export default HomeQuiz;
