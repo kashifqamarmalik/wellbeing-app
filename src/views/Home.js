4bc5d71b626d1c2fe41792f62a89fab9c329e628
Kashif <kashifqamar111@gmail.com>
2021-05-02 00:49:44 +0300

    added orange buttons on home page and graph and mood calendar and quiet days

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,19 +3,41 @@ import {Text, View, Button} from 'native-base';
 import CustomButton from '../components/CustomButton';
 import {Calendar} from 'react-native-plain-calendar';
 import {StyleSheet} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Home = ({navigation}) => {
   return (
     <View>
       <View style={styles.button}>
-        <CustomButton
-          title="Asses"
-          onPress={() => navigation.navigate('HomeQuiz')}
-        />
-        <CustomButton
-          title="History"
-          onPress={() => navigation.navigate('ViewQuiz')}
-        />
+        <TouchableOpacity
+          style={styles.buttonStyle}
+          onPress={() => navigation.navigate('HomeQuiz')}>
+          <Text style={styles.textStyle}>Asses</Text>
+          <Ionicons name="receipt" size={100} />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.buttonStyle1}
+          onPress={() => navigation.navigate('ViewQuiz')}>
+          <Text style={styles.textStyle}>History</Text>
+          <Ionicons name="time" size={100} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.button1}>
+        <TouchableOpacity
+          style={styles.buttonStyle}
+          onPress={() => navigation.navigate('HomeQuiz')}>
+          <Text style={styles.textStyle}>Quiet Days</Text>
+          <Ionicons name="mic-off" size={100} />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.buttonStyle1}
+          onPress={() => navigation.navigate('ViewQuiz')}>
+          <Text style={styles.textStyle}>Mood Calender</Text>
+          <Ionicons name="calendar" size={100} />
+        </TouchableOpacity>
       </View>
 
       <View style={styles.calendar}>
@@ -30,7 +52,35 @@ const styles = StyleSheet.create({
     marginTop: '30%',
   },
   button: {
-    marginTop: '10%',
+    margin: '10%',
+    flexDirection: 'row',
+    marginBottom: '1%',
+  },
+  button1: {
+    margin: '10%',
+    flexDirection: 'row',
+    marginTop: '1%',
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    backgroundColor: '#ff7b00',
+    width: 150,
+    height: 150,
+    padding: 5,
+    marginLeft: '5%',
+    borderRadius: 20,
+  },
+  buttonStyle1: {
+    alignItems: 'center',
+    backgroundColor: '#ff7b00',
+    width: 150,
+    height: 150,
+    padding: 5,
+    marginRight: '5%',
+    borderRadius: 20,
+  },
+  textStyle: {
+    fontWeight: 'bold',
   },
 });
 
